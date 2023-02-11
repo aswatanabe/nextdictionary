b29beb1dc27f3886e19a63a9f581275288e0f7de
aswatanabe <awatanabegithub@gmail.com>
2023-02-11 23:41:55 +0900

    ♻️ Change footer; text to component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from 'react';
 import { Header } from './header';
+import { Footer } from './footer';
 
 export function Layout({ children }: {children?: ReactNode;}) {
     return (
         <>
             <Header />
             <main>{children}</main>
-            <footer>FOOTER</footer>
+            <Footer />
         </>
     )
 }
