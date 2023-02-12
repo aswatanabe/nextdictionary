7dea3c105acaeb428decc616b6d37804c0cb5785
aswatanabe <awatanabegithub@gmail.com>
2023-02-12 15:54:19 +0900

    To use Google Fonts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+/* Google Fontsの有効化 */
+module.exports = {
+	experimental: {
+		optimizeFonts: true,
+	},
+};
