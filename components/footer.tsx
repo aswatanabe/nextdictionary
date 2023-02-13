c7aa3a1dca3d148d70d2158e5e68e63823ec1eb3
aswatanabe <awatanabegithub@gmail.com>
2023-02-14 03:09:18 +0900

    Change styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,15 @@
 import { Logo } from "./logo";
 import { SocialMedia } from "./socialMedia";
 import { FooterLink } from "./footerLink";
+import styles from 'styles/footer.module.css';
 
 export function Footer() {
     return (
-        <footer>
-            <Logo />
-            <SocialMedia />
+        <footer className={styles.flexContainer}>
+            <div >
+                <Logo />
+                <SocialMedia />
+            </div>
             <FooterLink />
         </footer>
     )
