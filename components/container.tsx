5eacd3b036cb6a7370ca9609258ea9fdf16d9372
aswatanabe <awatanabegithub@gmail.com>
2023-02-14 04:39:58 +0900

    Add type to avoid the error

--- components/container.tsx
+++ components/container.tsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import styles from 'styles/container.module.css';
 
-export function Container ({ children, large = false }) {
-    return (
+type Props={
+	children: React.ReactNode;
+	large?: boolean;
+};
+
+export function Container({ children, large = false }: Props) {
+	return (
 		<div className={large ? styles.large : styles.default}>
 			{children}
 		</div>
-	) 
+	);
 }
