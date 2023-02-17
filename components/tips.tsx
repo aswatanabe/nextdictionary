89ea571d6c31d44c237181826815c76879ff9366
aswatanabe <awatanabegithub@gmail.com>
2023-02-18 06:44:53 +0900

    Refactor the code of the tips component

--- components/tips.tsx
+++ components/tips.tsx
@@ -1,12 +1,24 @@
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import styles from "styles/tips.module.css";
+
 export function Tips() {
-    return (
-        <div>
-          <h2>Tips</h2>
-          <ul>
-            <li>発音に注意</li>
-            <li>be used to / get used to の違いに注意</li>
-            <li>used to 原形 / be used to -ing / be used to 原形 の違いに注意</li>
-          </ul>
-      </div>
-    )
+	const tips = [
+		"発音に注意",
+		"be used to / get used to の違いに注意",
+		"used to 原形 / be used to -ing / be used to 原形 の違いに注意"
+	];
+
+	return (
+		<div>
+			<h2>Tips</h2>
+			<ul>
+				{tips.map((tip) => (
+					<li key={tip}>
+						<AutoAwesomeIcon className={styles.listIcon} />
+						{tip}
+					</li>
+        		))}
+			</ul>
+		</div>
+	);
 }
