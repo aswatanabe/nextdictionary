fde30868975c651ca40f4656e62abe6546950bb7
aswatanabe <awatanabegithub@gmail.com>
2023-02-18 07:07:14 +0900

    Change the layout of the english component

--- components/english.tsx
+++ components/english.tsx
@@ -1,10 +1,12 @@
+import styles from "styles/english.module.css";
+
 export function English() {
     return (
         <div>
           <h2>英語説明</h2>
           <figure>
-            <blockquote>to have experienced something so that it no longer seems surprising, difficult, strange etc</blockquote>
-            <figcaption>出典：ロングマン現代英英辞典</figcaption>
+            <blockquote className={styles.blockquote}>to have experienced something so that it no longer seems surprising, difficult, strange etc</blockquote>
+            <figcaption className={styles.figcaption}>出典：ロングマン現代英英辞典</figcaption>
           </figure>
       </div>
     )
