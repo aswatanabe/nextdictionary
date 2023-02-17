2eb6b3b10e90cbac2ae64f8f59aebb06ae3457dd
aswatanabe <awatanabegithub@gmail.com>
2023-02-18 05:04:43 +0900

    Fix the layout of the keyword component

--- components/keyword.tsx
+++ components/keyword.tsx
@@ -1,9 +1,11 @@
+import styles from 'styles/keyword.module.css';
+
 export function Keyword() {
     return (
-        <div>
+        <div className={styles.sideBySide}>
             <h1>be used to</h1>
-            <p>助動詞</p>
-            <p>熟語</p>
-      </div>
+            <p className={`${styles.icon} ${styles.iconText}`}>助動詞</p>
+            <p className={`${styles.icon} ${styles.iconText}`}>熟語</p>
+        </div>
     )
 }
