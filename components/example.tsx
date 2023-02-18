5a9e73a28195e1e256497d30216da3a4cd62a897
aswatanabe <awatanabegithub@gmail.com>
2023-02-18 09:05:29 +0900

    Change the layout of the example component

--- components/example.tsx
+++ components/example.tsx
@@ -1,15 +1,26 @@
+import styles from "styles/example.module.css";
+
 export function Example() {
-    return (
-        <div>
-          <h2>例</h2>
-          <div>
-            <p>I&apos;m used to speaking English.</p>
-            <p>わたしは英語を話すことに慣れています。</p>
-          </div>
-          <div>
-            <p>The cat is not used to its new home yet.</p>
-            <p>そのネコはまだ新しい家に慣れていません。</p>
-          </div>
-      </div>
-    )
-}
+	const examples = [
+		{
+			en: "I'm used to speaking English.",
+			ja: "わたしは英語を話すことに慣れています。",
+		},
+		{
+			en: "The cat is not used to its new home yet.",
+			ja: "そのネコはまだ新しい家に慣れていません。",
+		},
+	];
+  
+	return (
+		<div>
+			<h2>例</h2>
+			{examples.map((example, index) => (
+				<div key={index} className={styles.example}>
+					<p>{example.en}</p>
+					<p>{example.ja}</p>
+				</div>
+			))}
+		</div>
+	);
+}  
