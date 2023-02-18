9244e8aa00d5100e2ef1b48dfd26674967527b8a
aswatanabe <awatanabegithub@gmail.com>
2023-02-18 09:26:43 +0900

    Fix code to avoid errors

--- components/ad.tsx
+++ components/ad.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 type AdProps = {
     height: 'sm' | 'md' | 'lg';
-};
+}
+
+type StyleProps = {
+    marginBlock: string;
+    inlineSize: string;
+    objectFit: 'contain' | 'cover' | 'fill' | 'none' | 'scale-down' | undefined;
+    aspectRatio: string;
+    backgroundColor: string;
+}
 
 export function Ad(props: AdProps) {
     const { height } = props;
@@ -10,19 +18,19 @@ export function Ad(props: AdProps) {
     let aspectRatio = '';
     switch (height) {
         case 'sm':
-        aspectRatio = '4/1';
+            aspectRatio = '4/1';
         break;
-    case 'md':
-        aspectRatio = '1/1';
+        case 'md':
+            aspectRatio = '1/1';
         break;
-    case 'lg':
-        aspectRatio = '1/2';
+        case 'lg':
+            aspectRatio = '1/2';
         break;
-    default:
+        default:
         break;
     }
 
-    const style = {
+    const style: StyleProps = {
         marginBlock: '1em',
         inlineSize: '100%',
         objectFit: 'cover',
