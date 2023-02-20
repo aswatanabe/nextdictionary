e4350a0b20950166e61b7e1a5e046c20e98cc741
aswatanabe <awatanabegithub@gmail.com>
2023-02-21 02:52:47 +0900

    Change the index component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,13 @@
-import { Container } from "@/components/container"
-import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "@/components/twoColumn"
-import { Paper } from "@mui/material"
-import { Search } from "@/components/search"
-import { Keyword } from "@/components/keyword"
-import { Tips } from "@/components/tips"
-import { Japanese } from "@/components/japanese"
-import { English } from "@/components/english"
-import { Description } from "@/components/description"
-import { Example } from "@/components/example"
-import { Note } from "@/components/note"
-import { More } from "@/components/more"
-import { Ad } from "@/components/ad"
+import { Search } from "@/components/search";
+import { Options } from "@/components/options";
+import styles from "styles/home.module.css";
 
 export default function Home() {
 	return (
-		<>
+		<div>
+			<h1 className={styles.title}>Next Dictionary</h1>
 			<Search />
-			<Container>
-				<TwoColumn>
-					<TwoColumnMain>
-						<Ad height="sm" />
-						<Keyword />
-						<Tips />
-						<Japanese />
-						<English />
-						<Description />
-						<Example />
-						<Ad height="md"/>
-						<Note />
-						<More />
-					</TwoColumnMain>
-					<TwoColumnSidebar>
-						<Ad height="md"/>
-						<Ad height="sm" />
-						<Ad height="md"/>
-						<Ad height="md"/>
-						<Ad height="lg"/>
-					</TwoColumnSidebar>
-				</TwoColumn>
-			</Container>
-		</>
+			<Options />
+		</div>
 	)
 }
