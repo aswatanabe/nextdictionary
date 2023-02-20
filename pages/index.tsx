4f9c27c578a34c762e761b1dc6ef853aec5c32b0
aswatanabe <awatanabegithub@gmail.com>
2023-02-21 03:38:21 +0900

    Refactor the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import { Search } from "@/components/search";
 import { Options } from "@/components/options";
-import styles from "styles/home.module.css";
+import styles from "styles/Home.module.css";
 
 export default function Home() {
 	return (
-		<div>
+		<div className={styles.wrapper}>
 			<h1 className={styles.title}>Next Dictionary</h1>
 			<Search />
 			<Options />
