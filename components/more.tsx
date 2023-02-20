da9c4d028f26bb5f8eebb2abaaa8e3144fdb9d76
aswatanabe <awatanabegithub@gmail.com>
2023-02-20 15:50:44 +0900

    Refactor / change styles of the more component

--- components/more.tsx
+++ components/more.tsx
@@ -1,9 +1,26 @@
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import styles from "styles/more.module.css";
+
+const more = [
+	{
+		id: 1,
+		text:"be used to / get used toを使わずに、現在完了形を使って「～に慣れている」を表現することもできる。",
+		example:"Don't worry. I've done this before.（心配しないで。これをするのは慣れているから）",
+	},
+];
+
 export function More() {
-    return (
-        <div>
-          <h2>More</h2>
-          <p>be used to / get used toを使わずに、現在完了形を使って「～に慣れている」を表現することもできる。</p>
-          <p>Don&apos;t worry. I&apos;ve done this before.（心配しないで。これをするのは慣れているから）</p>
-      </div>
-    )
+	return (
+		<div className={styles.captionBox}>
+			<h2 className={styles.caption}><TipsAndUpdatesIcon />More</h2>
+			<dl>
+				{more.map((item) => (
+					<div key={item.id.toString()}>
+						<dt>{item.text}</dt>
+						<dd className={styles.dd}>{item.example}</dd>
+					</div>
+				))}
+			</dl>
+		</div>
+	);
 }
