9cf84a38a9faeb471acd1865dc76dbdafc652a90
aswatanabe <awatanabegithub@gmail.com>
2023-02-20 18:59:22 +0900

    Make a hamburger menu

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,24 +1,50 @@
+import {useState} from "react";
 import Link from "next/link";
 import styles from 'styles/nav.module.css';
 
 export function Nav() {
+    const [navIsOpen, setNavIsOpen] = useState(false)
+
+    const toggleNav = () => {
+        setNavIsOpen((prev) => !prev)
+    }
+
+    const closeNav = () => {
+        setNavIsOpen (false)
+    }
+
     return (
-        <nav>
+        <nav className={navIsOpen ? styles.open : styles.close}>
+            {navIsOpen && (
+                <style jsx global>{`
+                    @media (max-width: 767px) {
+                        body {
+                            overflow: hidden;
+                            position: fixed;
+                            inline-size: 100%;
+                        }
+                    }
+                `}</style>
+            )}
+            <button className={styles.btn} onClick={toggleNav}>
+                <span className={styles.bar}></span>
+                <span className={styles.srOnly}>MENU</span>
+            </button>
             <ul className={styles.list}>
                 <li>
-                    <Link href="/">文法・語法</Link>
+                    <Link href="/" onClick={closeNav}>文法・語法</Link>
                 </li>
                 <li>
-                    <Link href="/">イディオム</Link>
+                    <Link href="/" onClick={closeNav}>イディオム</Link>
                 </li>
                 <li>
-                    <Link href="/">ボキャブラリー</Link>
+                    <Link href="/" onClick={closeNav}>ボキャブラリー</Link>
                 </li>
                 <li>
-                    <Link href="/">語源</Link>
+                    <Link href="/" onClick={closeNav}>語源</Link>
                 </li>
                 <li>
-                    <Link href="/">会話表現</Link>
+                    <Link href="/" onClick={closeNav}>会話表現</Link>
                 </li>
 
             </ul>
