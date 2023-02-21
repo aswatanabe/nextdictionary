49b96dcae4035c8d6cf69d4ca965964e7761feb1
aswatanabe <awatanabegithub@gmail.com>
2023-02-21 10:15:27 +0900

    Fix the code of the english component

--- components/english.tsx
+++ components/english.tsx
@@ -1,13 +1,18 @@
 import styles from "styles/english.module.css";
 
 export function English() {
+	const english = {
+		quote: "to have experienced something so that it no longer seems surprising, difficult, strange etc",
+		source: "ロングマン現代英英辞典",
+	}
+
     return (
         <div>
-          <h2>英語説明</h2>
-          <figure>
-            <blockquote className={styles.blockquote}>to have experienced something so that it no longer seems surprising, difficult, strange etc</blockquote>
-            <figcaption className={styles.figcaption}>出典：ロングマン現代英英辞典</figcaption>
-          </figure>
-      </div>
-    )
+			<h2>英語説明</h2>
+			<figure>
+				<blockquote className={styles.blockquote}>{english.quote}</blockquote>
+				<figcaption className={styles.figcaption}>出典：{english.source}</figcaption>
+			</figure>
+		</div>
+	)
 }
