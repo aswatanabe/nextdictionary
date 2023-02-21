b61c4111718520bf07a6fbe305e577f093686a57
aswatanabe <awatanabegithub@gmail.com>
2023-02-21 10:05:25 +0900

    Fix the code of  the japanese component

--- components/japanese.tsx
+++ components/japanese.tsx
@@ -1,8 +1,12 @@
 export function Japanese() {
-    return (
-        <div>
-          <h2>日本語訳</h2>
-          <p>be used to 名詞で「名詞に慣れている」</p>
-      </div>
-    )
+	const japanese = [
+		"be used to 名詞で「名詞に慣れている」",
+	];
+
+	return (
+		<div>
+			<h2>日本語訳</h2>
+			<p>{japanese}</p>
+		</div>
+	);
 }
