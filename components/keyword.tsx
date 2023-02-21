322fd55d70086ed9426724aedec3c1ac3bfde97a
aswatanabe <awatanabegithub@gmail.com>
2023-02-21 14:32:46 +0900

    Fix the code of the keyword component

--- components/keyword.tsx
+++ components/keyword.tsx
@@ -1,11 +1,20 @@
 import styles from 'styles/keyword.module.css';
 
 export function Keyword() {
+    const keyword = {
+        h1: "be used to",
+        p: [
+            "助動詞",
+            "熟語"
+        ]
+    };
+
     return (
         <div className={styles.sideBySide}>
-            <h1>be used to</h1>
-            <p className={`${styles.icon} ${styles.iconText}`}>助動詞</p>
-            <p className={`${styles.icon} ${styles.iconText}`}>熟語</p>
+            <h1>{keyword.h1}</h1>
+                {keyword.p.map((p, index) => (
+                    <p key={index} className={`${styles.icon} ${styles.iconText}`}>{p}</p>
+                ))}
         </div>
     )
 }
