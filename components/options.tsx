359897cfb0c8bfd3fbe7ffb4394b49e923ebb27a
aswatanabe <awatanabegithub@gmail.com>
2023-02-21 09:54:53 +0900

    Refactor the options component

--- components/options.tsx
+++ components/options.tsx
@@ -4,43 +4,65 @@ import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import styles from "styles/options.module.css";
 
+// export function Options() {
+// 	return (
+// 		<div>
+// 			<Box className={styles.box}>
+// 				<List className={styles.paper}>
+// 					<ListItem>
+// 						<ListItemButton component="a" href="#simple-list-A">
+// 							<ListItemText primary="AAA"/>
+// 						</ListItemButton>
+// 					</ListItem>
+// 					<Divider className={styles.divider}/>
+// 					<ListItem>
+// 						<ListItemButton component="a" href="#simple-list-B">
+// 							<ListItemText primary="BBB"/>
+// 						</ListItemButton>
+// 					</ListItem>
+// 					<Divider className={styles.divider}/>
+// 					<ListItem>
+// 						<ListItemButton component="a" href="#simple-list-C">
+// 							<ListItemText primary="CCC"/>
+// 						</ListItemButton>
+// 					</ListItem>
+// 					<Divider className={styles.divider}/>
+// 					<ListItem>
+// 						<ListItemButton component="a" href="#simple-list-D">
+// 							<ListItemText primary="DDD"/>
+// 						</ListItemButton>
+// 					</ListItem>
+// 					<Divider className={styles.divider}/>
+// 					<ListItem>
+// 						<ListItemButton component="a" href="#simple-list-E">
+// 							<ListItemText primary="EEE"/>
+// 						</ListItemButton>
+// 					</ListItem>
+// 					<Divider className={styles.divider}/>
+// 				</List>
+// 			</Box>
+// 		</div>
+// 	);
+// }
+
 export function Options() {
+	const listItems = ["AAA", "BBB", "CCC", "DDD", "EEE"];
 	return (
 		<div>
 			<Box className={styles.box}>
 				<List className={styles.paper}>
-					<ListItem>
-						<ListItemButton component="a" href="#simple-list">
-							<ListItemText primary="AAA"/>
-						</ListItemButton>
-					</ListItem>
-					<Divider className={styles.divider}/>
-					<ListItem>
-						<ListItemButton component="a" href="#simple-list">
-							<ListItemText primary="BBB"/>
-						</ListItemButton>
-					</ListItem>
-					<Divider className={styles.divider}/>
-					<ListItem>
-						<ListItemButton component="a" href="#simple-list">
-							<ListItemText primary="CCC"/>
-						</ListItemButton>
-					</ListItem>
-					<Divider className={styles.divider}/>
-					<ListItem>
-						<ListItemButton component="a" href="#simple-list">
-							<ListItemText primary="DDD"/>
-						</ListItemButton>
-					</ListItem>
-					<Divider className={styles.divider}/>
-					<ListItem>
-						<ListItemButton component="a" href="#simple-list">
-							<ListItemText primary="EEE"/>
-						</ListItemButton>
-					</ListItem>
-					<Divider className={styles.divider}/>
+					{listItems.map((item, index) => (
+						<React.Fragment key={index}>
+							<ListItem>
+								<ListItemButton component="a" href={`#simple-list-${item}`}>
+									<ListItemText primary={item} />
+								</ListItemButton>
+							</ListItem>
+							<Divider className={styles.divider} />
+						</React.Fragment>
+					))}
 				</List>
 			</Box>
 		</div>
 	);
-}
+}  
