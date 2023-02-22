f521bb6157e96c53c5716ad8929994c3159c9de6
aswatanabe <awatanabegithub@gmail.com>
2023-02-22 16:14:59 +0900

    Fix the code to use a json file

--- components/keyword.tsx
+++ components/keyword.tsx
@@ -1,20 +1,36 @@
 import styles from 'styles/keyword.module.css';
 
-export function Keyword() {
-    const keyword = {
-        h1: "be used to",
-        p: [
-            "助動詞",
-            "熟語"
-        ]
-    };
+import path from 'path';
 
-    return (
-        <div className={styles.sideBySide}>
-            <h1>{keyword.h1}</h1>
-                {keyword.p.map((p, index) => (
-                    <p key={index} className={`${styles.icon} ${styles.iconText}`}>{p}</p>
-                ))}
-        </div>
-    )
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data.json');
+  const res = await fetch(filePath);
+  const objectData = await res.json();
+
+  return {
+    props: {
+      keyword: objectData.data.keyword,
+    },
+  };
+}
+
+export function Keyword(props) {
+  const keyword = props.keyword;
+      // const keyword = {
+    //     h1: "be used to",
+    //     p: [
+    //         "助動詞",
+    //         "熟語"
+    //     ]
+    // };
+  return (
+    <div className={styles.sideBySide}>
+      <h1>{keyword.h1}</h1>
+      {keyword.p.map((p, index) => (
+        <p key={index} className={`${styles.icon} ${styles.iconText}`}>
+          {p}
+        </p>
+      ))}
+    </div>
+  );
 }
