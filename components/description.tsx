bfc2e8fe0fefbd0ce73ac3d55ce2b78e60785b21
aswatanabe <awatanabegithub@gmail.com>
2023-02-26 18:01:43 +0900

    Fix the code / change the directory to display an image in the description component

--- components/description.tsx
+++ components/description.tsx
@@ -1,15 +1,12 @@
 // import data from "../data.json";
 import styles from "styles/description.module.css";
 import Image from "next/image";
-import hello from "images/hello.jpg";
-
-
 
 export function Description({ imageOn = false }) {
 	const descriptions = {
-		text: "＜used（形容詞）＋to（前置詞）＋名詞＞の形。usedは「（人・動物・感覚器官が）（事・モノ・人）に慣れている」という意味を表す形容詞",
-		imageSrc: hello,
-		imageAlt: hello
+		text: "＜used（形容詞）＋to（前置詞）＋名詞＞の形。usedは「（人・動物・感覚器官が）（事・モノ・人）に慣れている」という意味を表す形容詞。",
+		imageSrc: "hello.jpg",
+		imageAlt: "Hello"
 	};
 
 	// const descriptions = data.data.descriptions;
@@ -17,8 +14,13 @@ export function Description({ imageOn = false }) {
 		<div>
 			<h2>解説</h2>
 			<p>{descriptions.text}</p>
-			<figure>
-				<Image src={descriptions.imageSrc} alt={descriptions.imageAlt} layout="responsive" className={styles.image} />
+			<figure className={styles.image}>
+				<Image
+					src={`/images/${descriptions.imageSrc}`}
+					alt={descriptions.imageAlt}
+					layout="fill"
+					objectFit="cover"
+				/>
 			</figure>
 		</div>
 	);
