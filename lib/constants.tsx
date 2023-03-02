ec810bbd3c42ad36077549745075fabbaed52179
aswatanabe <awatanabegithub@gmail.com>
2023-03-03 06:24:55 +0900

    Make metadata

---
+++ lib/constants.tsx
@@ -0,0 +1,9 @@
+export const siteMeta = {
+	siteTitle: "Next Dictionary",
+	sideDesc: "日本人が知っておくべき文法・語法・イディオム・ボキャブラリー・語源・会話表現の検索サイト。",
+	siteUrl: "",
+	siteLang: "ja",
+	siteLocale: "ja_JP",
+	siteType: "website",
+	siteIcon: "",
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { siteMeta } from "@/lib/constants";
+const { siteLang } = siteMeta;
+
 export default function Document() {
 	return (
-		<Html lang="ja">
+		<Html lang={siteLang}>
 			<Head>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans:wght@200;400;700;900&display=swap"
