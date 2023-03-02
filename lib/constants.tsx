b59338e6fc839ad0348503d363bf5f1c4c1dea53
aswatanabe <awatanabegithub@gmail.com>
2023-03-03 07:29:02 +0900

    Fix the error

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,6 +1,6 @@
 export const siteMeta = {
 	siteTitle: "Next Dictionary",
-	sideDesc: "日本人が知っておくべき文法・語法・イディオム・ボキャブラリー・語源・会話表現の検索サイト。",
+	siteDesc: "日本人が知っておくべき文法・語法・イディオム・ボキャブラリー・語源・会話表現の検索サイト。",
 	siteUrl: "",
 	siteLang: "ja",
 	siteLocale: "ja_JP",
